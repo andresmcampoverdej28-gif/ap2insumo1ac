84217a65e2bf431704a510008687763a9368f325
andresm.campoverdej.28 <andresm.campoverdej.28@gmail.com>
2025-11-08 23:57:31 +0000

    ✨feat(_layout.tsx): Se agrego codigo para las cabeceras

--- app/Profile/_layout.tsx
+++ app/Profile/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import React from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function RootLayout(){
+    return(
+        <Stack screenOptions={{
+            headerShown:false
+        }}>
+            <Stack.Screen name="index"></Stack.Screen>
+            <Stack.Screen name="(main)" options={{
+                headerShown: false
+            }}></Stack.Screen>
+        </Stack>
+    );    
 }
